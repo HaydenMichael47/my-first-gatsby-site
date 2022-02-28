d30fc2023dae5941a5ffb0f03d7132d6c9714e34
Hayden Michael <haydentmichael@gmail.com>
2022-02-28 18:22:19 -0500

    Problems with Gatsby tutorial part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,20 +3,20 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
-    return (
-      <Layout pageTitle="My Blog Posts">
-        <ul>
-        {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-            </li>
-          ))
-        }
-        </ul>
-      </Layout>
-    )
-  }
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query {
